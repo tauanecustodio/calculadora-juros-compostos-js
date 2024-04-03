c9b5f4f1e8fed6354399828aaeff07de8f973d84
Tauane.Custodio <tauanesantoscustodio@gmail.com>
2024-04-03 18:39:30 -0300

    update

--- assetss/js/script.js
+++ assetss/js/script.js
@@ -1,27 +1,21 @@
-const buttonCalcular = document.getElementById('calcular');
-const buttonLimpar = document.getElementById('limpar');
 const textResult = document.getElementById('text-result');
 
-
-buttonCalcular.addEventListener('click', function (event) {
+document.getElementById('calcular').addEventListener('click', function (event) {
     event.preventDefault();
     
     const capital = parseFloat(document.getElementById('capital').value);
     const juros = parseFloat(document.getElementById('juros').value) / 100;
     const tempo = parseFloat(document.getElementById('tempo').value);
+    
     const montante = capital * Math.pow(1 + juros, tempo);
 
-    console.log(capital);
-    console.log(juros);
-    console.log(tempo);
-    console.log(montante);
+    textResult.innerText = "R$ " + montante.toFixed(2).replace('.',',');
 
-    textResult.innerText = montante.toFixed(2);
 });
 
-buttonLimpar.addEventListener('click', function () {
+document.getElementById('limpar').addEventListener('click', function () {
     document.getElementById('capital').value = '';
     document.getElementById('juros').value = '';
     document.getElementById('tempo').value = '';
-    textResult.innerText = "0.00";
+    textResult.innerText = "R$ 0,00";
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# CALCULADORA DE RENDIMENTOS COM JUROS COMPOSTOS
+
+![alt text](image.png)
+
+Fonte: [Calculadora de Juros Compostos | HTML, CSS e JAVASCRIPT](https://youtu.be/9ImvH4bzr-k?si=ceUJG9ic1aOQBCoh).
